d4606226cc5a9dc7ac3b3e15e0da93e55a041128
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-12-12 17:58:55 +0600

    update

--- lib/controller/language_controller.dart
+++ lib/controller/language_controller.dart
@@ -1,78 +1,8 @@
-// import 'dart:ui';
-// import 'package:get/get.dart';
-// import 'package:language_change_test/app_constants.dart';
-// import 'package:language_change_test/models/language_model.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
-//
-// class LocalizationController extends GetxController implements GetxService {
-//   final SharedPreferences sharedPreferences;
-//
-//   LocalizationController({required this.sharedPreferences}) {
-//     loadCurrentLanguage();
-//   }
-//
-//   Locale _locale = Locale(AppConstants.languages[0].languageCode,
-//       AppConstants.languages[0].countryCode);
-//
-//   int _selectedIndex = 0;
-//
-//   int get selectedIndex => _selectedIndex;
-//   List<LanguageModel> _languages = [];
-//
-//   Locale get locale => _locale;
-//
-//   List<LanguageModel> get languages => _languages;
-//
-//   void loadCurrentLanguage() {
-//     _locale = Locale(
-//         sharedPreferences.getString(AppConstants.LANGUAGE_CODE) ??
-//             AppConstants.languages[0].languageCode,
-//         sharedPreferences.getString(AppConstants.COUNTRY_CODE) ??
-//             AppConstants.languages[0].countryCode);
-//
-//     for (int index = 0; index < AppConstants.languages.length; index++) {
-//       if (AppConstants.languages[index].languageCode == _locale.languageCode) {
-//         _selectedIndex = index;
-//         break;
-//       }
-//     }
-//     _languages = [];
-//     _languages.addAll(AppConstants.languages);
-//     update();
-//     print('Load language');
-//   }
-//
-//   void setLanguage(Locale locale) {
-//     Get.updateLocale(locale);
-//     _locale = locale;
-//     saveLanguage(_locale);
-//     print(_locale);
-//     update();
-//     print(_locale);
-//   }
-//
-//   void setSelectedIndex(int index) {
-//     _selectedIndex = index;
-//     update();
-//   }
-//
-//   void saveLanguage(Locale locale) async {
-//     sharedPreferences.setString(
-//         AppConstants.LANGUAGE_CODE, locale.languageCode);
-//     sharedPreferences.setString(AppConstants.COUNTRY_CODE, locale.countryCode!);
-//   }
-// }
-
-
-
-
 import 'dart:ui';
-
 import 'package:get/get.dart';
 import 'package:language_change_test/app_constants.dart';
 import 'package:language_change_test/models/language_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 
 class LocalizationController extends GetxController implements GetxService {
   final SharedPreferences sharedPreferences;
@@ -85,15 +15,18 @@ class LocalizationController extends GetxController implements GetxService {
       AppConstants.languages[0].countryCode);
 
   int _selectedIndex = 0;
+
   int get selectedIndex => _selectedIndex;
   List<LanguageModel> _languages = [];
+
   Locale get locale => _locale;
+
   List<LanguageModel> get languages => _languages;
 
   void loadCurrentLanguage() {
-    // only gets called during installation or rebooting
-    _locale = Locale(sharedPreferences.getString(AppConstants.LANGUAGE_CODE) ??
-        AppConstants.languages[0].languageCode,
+    _locale = Locale(
+        sharedPreferences.getString(AppConstants.LANGUAGE_CODE) ??
+            AppConstants.languages[0].languageCode,
         sharedPreferences.getString(AppConstants.COUNTRY_CODE) ??
             AppConstants.languages[0].countryCode);
 
@@ -106,13 +39,16 @@ class LocalizationController extends GetxController implements GetxService {
     _languages = [];
     _languages.addAll(AppConstants.languages);
     update();
+    print('Load language');
   }
 
   void setLanguage(Locale locale) {
     Get.updateLocale(locale);
     _locale = locale;
     saveLanguage(_locale);
+    print(_locale);
     update();
+    print(_locale);
   }
 
   void setSelectedIndex(int index) {
@@ -125,5 +61,4 @@ class LocalizationController extends GetxController implements GetxService {
         AppConstants.LANGUAGE_CODE, locale.languageCode);
     sharedPreferences.setString(AppConstants.COUNTRY_CODE, locale.countryCode!);
   }
-
 }
